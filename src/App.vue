8fe09963faa226a26ee58e69c5e7f466160b540c
白帝子 <11704321+baidizi@user.noreply.gitee.com~>
2023-09-07 14:07:22 +0800

    ‘配置elrment-plus’

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <template>
-  <div>我是APP页面</div>
+  <div>
+    <el-button type="primary" size="default" :icon="Plus">主要按钮</el-button>
+  </div>
 </template>
 
 <script lang="ts" setup>
+// 引入图标组件
+import { Plus } from '@element-plus/icons-vue'
 for (let i = 0; i < 100; i++) {
   console.log(i)
 }
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
-createApp(App).mount('#app')
-
-const a = 100
-
-//测试代码
-const fun = () => {
-  console.log('我爱我的祖国')
-}
-fun()
+// 引入element-plus的插件与样式
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+// 配置element-plus国际化
+//@ts-ignore 忽略当前文件ts类型的检测否则有红色提示(打包会失败)
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+// 获取应用实例对象
+const app = createApp(App)
+// 将应用挂载到挂载点上
+app.use(ElementPlus, {
+  locale: zhCn, //element-plus国际化配置
+})
+app.mount('#app')
