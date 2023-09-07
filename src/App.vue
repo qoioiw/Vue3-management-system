d5e04325c3a92cdfc7715219c7a22362a33e616e
白帝子 <11704321+baidizi@user.noreply.gitee.com~>
2023-09-07 12:27:36 +0800

    提交格式化代码

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>我是APP页面</div>
+</template>
+
+<script lang="ts" setup>
+for(let i = 0;i<100;i++)
+{
+  console.log(i);
+}
+</script>
+
+<style></style>
---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
+
+const a = 100
+
+//测试代码
+const fun = () => {
+  console.log('我爱我的祖国')
+}
+fun()
